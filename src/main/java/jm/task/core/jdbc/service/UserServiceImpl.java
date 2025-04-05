d1e241b84578260b22619cabbaf2b27e2bb141d3
Michael Mark <selfdevourment@gmail.com>
2025-04-05 22:22:48 +0500

    Change UserDao field to interface.

--- src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -1,11 +1,12 @@
 package jm.task.core.jdbc.service;
 
+import jm.task.core.jdbc.dao.UserDao;
 import jm.task.core.jdbc.dao.UserDaoHibernateImpl;
 import jm.task.core.jdbc.model.User;
 import java.util.List;
 
 public class UserServiceImpl implements UserService {
-    public final UserDaoHibernateImpl userDao = new UserDaoHibernateImpl();
+    public final UserDao userDao = new UserDaoHibernateImpl();
 
     @Override
     public void createUsersTable() {
